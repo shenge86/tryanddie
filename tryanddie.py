8f26bf05059a853dbbf983cec9dd07d50a107d91
shenge86 <shenge86@gmail.com>
2021-02-19 22:07:01 -0600

    add death states

--- tryanddie.py
+++ tryanddie.py
@@ -18,11 +18,46 @@ class Person():
         self._energy = val
         print("My energy feels like it has changed.")
         print("Energy: ", self._energy)
+        
+        if self.energy <= 5:
+            print("I am starting to feel weak.")
+            
         if self.energy <= 0:
             print("Oh my god! I feel faint. I think I might be ...")
             print("DEAD!")
+            
+    def reset(self):
+        print("I suddenly feel strength flowing through my body again.")
+        print("*Gasp* I am alive!")
+        self._name = 'Shen'
+        self._energy = 10
+        return self
 
-
+class Enviro():
+    def __init__(self,deathstate):
+        self.deathstate = 0
+        self.person = Person('Shen',10)
+        
+    @property
+    def deathstate(self):
+        return self._deathstate
+    
+    @deathstate.setter
+    def deathstate(self,val):
+        self._deathstate = val
+        
+    @property
+    def personenergy(self):
+        return self.person._energy
+    
+    @personenergy.setter
+    def personenergy(self,val):
+        self.person.energy = val
+        if self.person.energy <= 0:
+            print("How many times have I died now?")
+            self._deathstate += 1
+            print("Oh, ", self.deathstate)
+            self.person = self.person.reset()
 
 print("---------------------------------------------------------")
 print("Welcome to Try and Die - A Text Game of Possible Death")        
@@ -31,9 +66,9 @@ print("'What the hell? Is this a dream?' I asked myself.")
     
 
 if __name__ == '__main__':
-    shen = Person('Shen',10)
-    print('Current energy: ', shen.energy)
-    shen.energy -= 10
+    # shen = Person('Shen',10)
+    # print('Current energy: ', shen.energy)
+    enviro = Enviro(0)
     
     loop = 1
     while True:
@@ -44,11 +79,14 @@ if __name__ == '__main__':
             else:
                 print("---------------------------------------------------------")
                 
-            if input_one.lower() in ['shout', 'yell']:
+            if input_one.lower() in ['shout', 'yell', 'scream']:
                 print("I yelled in fear and confusion! The echoes came back. There was no other response.")
                 print("Wow, that was quite the waste of energy, I thought.")
+                # enviro.person.energy -= 1
+                enviro.personenergy -=6
             elif input_one.lower() in ['eat mushroom']:
-                print("I ate the mushroom and I instantly started shrinking.")
+                print("I ate the mushroom and I instantly started shrinking. Also, I feel a bit stronger.")
+                enviro.personenergy +=1
                 print("Now that I am the size of a mouse, I can see that in front of me there is a small tunnel.")
                 print("It is possibly made by a small rodent.")
                 loop=2
